c00084e6617a1456a264f045441b010e27ec5710
Heroic Mastodon <heroicmastodon@gmail.com>
2018-05-30 11:45:37 -0600

    L 133 Finding Screen Pixel Coordinates

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -37,7 +37,7 @@ void ATankPlayerController::Tick(float DeltaTime)
 	 FVector HitLocation;  // Out parameter
 	 if (GetSightRayHitLocation(HitLocation))
 	 {
-		 UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())  // Has "side-effect", is going to line trace
+		// UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString())  // Has "side-effect", is going to line trace
 		// TODO Tell controlled tank to aim at this point
 	 }
  }
@@ -45,6 +45,12 @@ void ATankPlayerController::Tick(float DeltaTime)
  // Get world location of linetrace through crosshair, true if hits landscape
  bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
  {
-	 OutHitLocation = FVector(1.0);
+	// Find the crosshair position
+	 int32 ViewportSizeX, ViewportSizeY;
+	 GetViewportSize(ViewportSizeX, ViewportSizeY);
+	 auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	// "De-project" the screen position of the crosshair to a world direction
+	// Line-trace along that look direction, and see what we hit (up to max range)
 	 return true;
  }
