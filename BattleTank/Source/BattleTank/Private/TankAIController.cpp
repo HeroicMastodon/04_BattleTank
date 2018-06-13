f2da2d8547edf06a3e0023837aa3dbf31fc87a40
Heroic Mastodon <heroicmastodon@gmail.com>
2018-06-13 13:40:23 -0600

    160 Setup Track Throttles

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -26,7 +26,7 @@ void ATankAIController::Tick(float DeltaTime)
 		// Aim towards the player
 		ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
-		ControlledTank->Fire();  // TODO limit firing rate
+		ControlledTank->Fire();  
 	}
 };
 
---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+
+
+// Used to set max driving force, and to apply forces to the Tank
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	
+};
