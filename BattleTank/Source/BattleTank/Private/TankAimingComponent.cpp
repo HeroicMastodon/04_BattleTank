83ef8623a829597e412d56d9388b2a4aa9648629
Heroic Mastodon <heroicmastodon@gmail.com>
2018-07-01 23:31:19 -0600

    mid lecture commit

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -115,7 +115,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	//UE_LOG(LogTemp, Warning, TEXT("AimASRotator: %s"), *AimAsRotator.ToString())
 
 	Barrel->Elevate(DeltaRotator.Pitch); 
-	if (DeltaRotator.Yaw > 180) { DeltaRotator.Yaw = -DeltaRotator.Yaw; }
+	if (FMath::Abs(DeltaRotator.Yaw) > 180) { DeltaRotator.Yaw = -DeltaRotator.Yaw; }
 	Turret->Rotate(DeltaRotator.Yaw);
 }
 
