474add7773d3b53d30efa7a9276674af740b6282
Heroic Mastodon <heroicmastodon@gmail.com>
2018-12-29 04:14:12 -0700

    Began Lecture 216

---
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -0,0 +1,28 @@
+// CopyRight Assorted Folk LTD
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SprungWheel.generated.h"
+
+UCLASS()
+class BATTLETANK_API ASprungWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASprungWheel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
