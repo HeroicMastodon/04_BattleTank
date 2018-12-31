5b5d382c3fe781d3e5ff6ea77365791099e08281
Heroic Mastodon <heroicmastodon@gmail.com>
2018-12-31 01:30:44 -0700

    Lecture 219

--- BattleTank/Source/BattleTank/Private/SprungWheel.cpp
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -12,10 +12,6 @@ ASprungWheel::ASprungWheel()
 	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassWheelConstraint"));
 	SetRootComponent(MassWheelConstraint);
 
-	
-
-	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
-	Mass->SetupAttachment(MassWheelConstraint);
 
 	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
 	Wheel->SetupAttachment(MassWheelConstraint);
@@ -28,16 +24,7 @@ void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
 
-	if (GetAttachParentActor())
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Not Null: %s"), *GetAttachParentActor()->GetName());
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Null"));
-	}
-	
-
+	SetupConstraint();
 }
 
 // Called every frame
@@ -47,3 +34,16 @@ void ASprungWheel::Tick(float DeltaTime)
 
 }
 
+void ASprungWheel::SetupConstraint()
+{
+	if (!GetAttachParentActor())
+	{
+		return;
+	}
+	UPrimitiveComponent* BodyRoot = Cast<UPrimitiveComponent>(GetAttachParentActor()->GetRootComponent());
+	if (!BodyRoot)
+	{
+		return;
+	}
+	MassWheelConstraint->SetConstrainedComponents(BodyRoot, NAME_None, Wheel, NAME_None);
+}
